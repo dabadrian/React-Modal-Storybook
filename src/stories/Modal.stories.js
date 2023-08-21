949ed9a60f517f1b86cf6f68cc81fc185e0f973e
Daniel Blanco <danielba.developer@gmail.com>
2023-08-21 00:15:28 -0400

    4th commit

---
+++ src/stories/Modal.stories.js
@@ -0,0 +1,15 @@
+import { Modal } from "./Modal.jsx";
+
+export default{
+    title: 'Example/Modal',
+    component: Modal
+};
+
+export const Primary = {
+    args:{
+        title: 'Confirmación',
+        children: 'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí".'
+    }
+    
+
+}
